af6992de9ec2b2fd1e4b1c26c8cc86d417a6b7b5
Jason Tucker <jason.tucker@locbit.com>
2017-11-02 14:05:43 -0700

    again

--- octoprint_SD3D/__init__.py
+++ octoprint_SD3D/__init__.py
@@ -681,7 +681,6 @@ class SD3DPlugin(octoprint.plugin.StartupPlugin,
 
                 self._auto_provision_printer()
 
-                self.edge_check()
 
                 def slice_monkey_patch_gen(slice_func):
                         def slice_monkey_patch(*args, **kwargs):
@@ -726,18 +725,18 @@ class SD3DPlugin(octoprint.plugin.StartupPlugin,
                         return slice_monkey_patch
 
                 octoprint.slicing.SlicingManager.slice = slice_monkey_patch_gen(octoprint.slicing.SlicingManager.slice)
+
                 edge_path = '/home/pi/oprint/lib/python2.7/site-packages/octoprint_SD3D/locbit-edge'
                 path_check = os.path.isdir(edge_path)
                 edge_url = '/usr/bin/git clone https://github.com/Locbit/locbit-edge.git ~/oprint/lib/python2.7/site-packages/octoprint_SD3D'
 
-                if self._settings.get(['macaddress']) is not None:
-                        while path_check != True:
-                                subprocess.call(edge_url.format(), shell=True)
-                                print("THIS BETTER WORK!")
-                                return self._logger.info(path_check)
+                while path_check != True:
+                        subprocess.call(edge_url.format(), shell=True)
+                        print("THIS BETTER WORK!")
+                        return self._logger.info(path_check)
                                 
-                        else:
-                                return
+                else:
+                        return
 
 	def get_settings_defaults(self):
 		return dict(did='',
@@ -925,6 +924,6 @@ class SD3DPlugin(octoprint.plugin.StartupPlugin,
                         return True
 
                 print('5' * 20 + "{}".format(self._settings.get(['macAddress'])))
-                
+
 __plugin_name__ = "Filtracker"
 __plugin_implementation__ = SD3DPlugin()
